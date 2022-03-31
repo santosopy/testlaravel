9a208a2f28bea717faaa7b784ff17e521e6c5f90
santoso <santoso@tsart.jp>
2022-03-31 10:46:17 +0900

    first commit

---
+++ database/migrations/2022_03_30_075039_create_vendors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("adress");
+            $table->string("city");
+            $table->string("state country");
+            $table->string("pincode");
+            $table->string("mobile");
+            $table->string("email")->unique();
+            $table->tinyInteger("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::prefix("admin")->namespace("App\Http\Controllers\Admin")->group(function(){
+    Route::match(["get","post"], 'login', "AdminController@login");
+    Route::group(["middleware"=>["admin"]], function(){
+        Route::get('dashboard', "AdminController@dashboard");
+    });
+});
